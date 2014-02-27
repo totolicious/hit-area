a80e02f99ebe329293f885eac172f8a122f9d1e1
Gabriel Totoliciu <gabriel@totoliciu.ro>
2014-02-27 03:05:46 +0200

    Don't trigger click if mouse moved or mouseup takes too long

--- hit-area.js
+++ hit-area.js
@@ -1,3 +1,30 @@
-$("body").on("click", ".hit-area", function () {
-	window.location=$(this).find("a").attr("href");
-});
+(function() {
+	$(document).ready(function() {
+		var click_timeout_threshold = 200,
+			move_pixels_threshold = 50,
+			start_time,
+			start_x,
+			start_y;
+
+		function start(event) {
+			start_time = new Date().getTime();
+			start_x = event.pageX;
+			start_y = event.pageY;
+		}
+
+		function stop(event) {
+			
+			if (new Date().getTime() - start_time < click_timeout_threshold
+				&& Math.abs(start_x - event.pageX) < move_pixels_threshold
+				&& Math.abs(start_y - event.pageY) < move_pixels_threshold) {
+				window.location=$(this).find("a").attr("href");
+			}
+
+		}
+
+		$("body").on("touchstart.hitarea mousedown.hitarea", ".hit-area", start);
+
+		$("body").on("touchend.hitarea mouseup.hitarea", ".hit-area", stop);
+	});
+
+})()
